f863f56775bf84b96626181481642c9c1ffe16a6
Chris Matthieu <chris@matthieu.us>
2016-04-07 21:23:57 -0700

    added echo function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
-  list: ["sentiment", "md5cracker", "nextprime"],
-  sentiment:  "https://raw.githubusercontent.com/computes/operations/master/sentiment/index.js",
+  list: ["echo", "md5cracker", "nextprime", "sentiment"],
+  echo:  "https://gist.githubusercontent.com/computes/df86808c4a9d0a0d489a/raw/11c92b86662a4df5b5db585a1442796333bd1934/test.js",
   md5cracker: "https://raw.githubusercontent.com/computes/operations/master/md5cracker/index.js",
-  nextprime: "https://raw.githubusercontent.com/computes/operations/master/nextprime/index.js"
+  nextprime: "https://raw.githubusercontent.com/computes/operations/master/nextprime/index.js",
+  sentiment:  "https://raw.githubusercontent.com/computes/operations/master/sentiment/index.js"
 };
