b6049ad45ac937e350f33713866d303c1480da08
Chris Matthieu <chris@matthieu.us>
2016-06-08 11:25:30 -0700

    added censor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   list: ["censor", "echo", "md5cracker", "nextprime", "sentiment"],
-  censor: "https://gist.githubusercontent.com/computes/6d3659a428863dd6c5d9903c9f851a0a/raw/cb83da487913f0d20bd16b2670418d5fe8d59449/censorfunction.js",
+  censor: "https://gist.githubusercontent.com/computes/6d3659a428863dd6c5d9903c9f851a0a/raw/4a971bab2205699a5b782c04c213459008e0680f/censorfunction.js",
   echo:  "https://gist.githubusercontent.com/computes/df86808c4a9d0a0d489a/raw/11c92b86662a4df5b5db585a1442796333bd1934/test.js",
   md5cracker: "https://raw.githubusercontent.com/computes/operations/master/md5cracker/index.js",
   nextprime: "https://raw.githubusercontent.com/computes/operations/master/nextprime/index.js",
